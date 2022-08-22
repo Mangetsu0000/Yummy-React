b33c91ee013b0756902a0acb68d78b287ac55682
Mouelhi Mohamed Fakhreddine <m.mouelhi0603@myu.universitecentrale.tn>
2022-08-22 15:02:53 +0100

    Updated Add

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,17 +5,22 @@ import { useRef } from "react";
 import { v4 as uuidv4 } from "uuid";
 const Form = ({ setData }) => {
   const recipeNameRef = useRef();
+  const recipeRatingRef = useRef();
   const handleClick = () => {
     const recipeName = recipeNameRef.current.value;
-    const newRecipe = { name: recipeName, id: uuidv4() };
-    console.log(newRecipe);
+    const recipeRating = recipeRatingRef.current.value;
 
-    setData((data) => ({...data, ({ name: recipeName, id: uuidv4() }) }));
+    setData((data) => [
+      ...data,
+      { name: recipeName, id: uuidv4(), rating: recipeRating },
+    ]);
     recipeNameRef.current.value = "";
+    recipeRatingRef.current.value = "";
   };
   return (
     <Card>
-      <input ref={recipeNameRef} type="text" />
+      <input ref={recipeNameRef} type="text" placeholder="recipe name" />
+      <input ref={recipeRatingRef} type="number" placeholder="recipe rating" />
       <button onClick={handleClick}>Add Recipe</button>
     </Card>
   );
